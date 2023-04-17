9aa2958405434ea0576db9f1268a4ed332c9ada3
omarkhan03 <omarahmadkhan2003@gmail.com>
2023-04-16 20:25:10 -0600

    Made a thing show up

--- src/script.js
+++ src/script.js
@@ -2,85 +2,68 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader.js'
-import { MeshLine, MeshLineMaterial, MeshLineRaycast } from 'three.meshline';
-
-import * as dat from 'lil-gui'
-import { BufferGeometry } from 'three'
-
-const floor5 = document.getElementsByClassName('f5')[0]
-const floor4 = document.getElementsByClassName('f4')[0]
-const floor3 = document.getElementsByClassName('f3')[0]
-const floor2 = document.getElementsByClassName('f2')[0]
-const floor1 = document.getElementsByClassName('f1')[0]
-const reset = document.getElementsByClassName('reset')[0]
-
-
-/**
- * Base
- */
-// Debug
-const gui = new dat.GUI()
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.getElementById('webgl')
 
 // Scene
 const scene = new THREE.Scene()
 
 // Loading ICT model 
-var loader = new ColladaLoader();
-loader.load("/models/ICT/ICT_COLLADA.dae", function (result) {
+// var loader = new ColladaLoader();
+// loader.load("/models/ICT/ICT_COLLADA.dae", function (result) {
 
-    const meshes = result.scene.children[0].children
+//     const meshes = result.scene.children[0].children
 
-    for(const mesh of meshes) {
-        const geometry = mesh.geometry
-        console.log(geometry)
-        const edges = new THREE.EdgesGeometry(geometry)
-        const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0x000000, linewidth: 100 }))
-        result.scene.add(line);
-    }
+//     for(const mesh of meshes) {
+//         const geometry = mesh.geometry
+//         console.log(geometry)
+//         const edges = new THREE.EdgesGeometry(geometry)
+//         const line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({ color: 0x000000, linewidth: 100 }))
+//         result.scene.add(line);
+//     }
 
-    scene.add(result.scene);
-});
+//     scene.add(result.scene);
+// });
 
-// Selected floor
-const geometry = new THREE.BoxGeometry(130,5,70)
-const material = new THREE.MeshBasicMaterial({ color: 0x0000ff, opacity: 0.2, transparent: true})
-const selected = new THREE.Mesh(geometry, material)
-selected.position.set(-11, 9, -8)
-scene.add(selected)
-selected.visible = false
+// Instantiate a loader
+const loader = new GLTFLoader();
 
-floor5.addEventListener('click', () => {
-    console.log('f5')
-    selected.visible = true
-    selected.position.set(-11, 30, -8)
-})
+// Optional: Provide a DRACOLoader instance to decode compressed mesh data
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath( '/examples/jsm/libs/draco/' );
+loader.setDRACOLoader( dracoLoader );
 
-floor4.addEventListener('click', () => {
-    console.log('f4')
-    selected.visible = true
-    selected.position.set(-11, 23, -8)
-})
+// Load a glTF resource
+loader.load(
+	// resource URL
+	'models/untitled.glb',
+	// called when the resource is loaded
+	function ( gltf ) {
 
-floor3.addEventListener('click', () => {
-    console.log('f3')
-    selected.visible = true
-    selected.position.set(-11, 16, -8)
-})
+		scene.add( gltf.scene );
 
-floor2.addEventListener('click', () => {
-    console.log('f2')
-    selected.visible = true
-    selected.position.set(-11, 9, -8)
-})
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
 
-floor1.addEventListener('click', () => {
-    console.log('f1')
-    selected.visible = true
-    selected.position.set(-11, 2, -8)
-})
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
 
 /**
  * Lights
@@ -103,15 +86,15 @@ scene.add(directionalLight)
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+    width: window.innerWidth/4,
+    height: window.innerHeight/4
 }
 
 window.addEventListener('resize', () =>
 {
     // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+    sizes.width = window.innerWidth/4
+    sizes.height = window.innerHeight/4
 
     // Update camera
     camera.aspect = sizes.width / sizes.height
@@ -127,24 +110,17 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000000)
-camera.position.set(-78.6, 32, 45)
+camera.position.set(0,-1,0)
 
-camera.rotation.set(-0.06, -0.76, 0.04)
+camera.rotation.set(0,0,0)
 
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
-controls.target.set(-10, 0, -10)
+controls.target.set(0,0,0)
 controls.enableDamping = true
 controls.saveState()
-
-
-// Resets camera rotation + selected floor
-reset.addEventListener('click', () => {
-    controls.reset()
-    selected.visible = false
-})
 
 /**
  * Renderer
